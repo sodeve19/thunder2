84c85a7805326bc87089113a3d155625f1c60b19
Oscar Rendon <orendon@gmail.com>
2014-10-10 07:58:02 -0500

    Implement basic like match

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,21 +2,22 @@ class FriendsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    pick_random_friend
+    @friend = current_user.random_friend
     # render :index (default behavior)
   end
 
   def like
-    liked = params[:liked] == "true"
-    current_user.likes.create(friend_id: params[:id], liked: liked)
+    # get data from like or unlike button
+    @friend         = User.find(params[:id])
+    like_or_unlike  = params[:liked] == "true"
 
-    pick_random_friend
-    render :index
-  end
+    # save like data into database
+    current_user.likes.create(friend_id: @friend.id, liked: like_or_unlike)
 
-  private
-
-  def pick_random_friend
-    @friend = current_user.random_friend
+    if @friend.liked?(current_user)
+      render :match
+    else
+      redirect_to root_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,13 @@ class User < ActiveRecord::Base
 
   has_many :likes
 
-  # All users I haven't like or unlike yet
+  # A random user I haven't like or unlike yet
   def random_friend
     restricted_ids = self.likes.pluck(:friend_id) << self.id
     User.where.not(id: restricted_ids).order("RANDOM()").first
+  end
+
+  def liked?(user)
+    likes.find_by_friend_id(user.id)
   end
 end
