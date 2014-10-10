54056c5ef82889dad1ef48b8247895d02f60233c
Oscar Rendon <orendon@gmail.com>
2014-10-10 04:20:09 -0500

    Show random friend details

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,5 +2,6 @@ class FriendsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @friend = current_user.random_friend
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def random_friend
+    User.where.not(id: self).order("RANDOM()").first
+  end
 end
